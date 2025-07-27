ca16c821e37fb519c723c72faf44ad71bc523cc4
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2025-07-27 11:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (26.82%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,28 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums=  new int[m+n]; 
-       int i=0, j=0,k=0;
-            while (i< m && j< n){
-                if(nums1[i]<nums2[j]){
-                    nums[k++]=nums1[i++];    
-                }
-                else  {
-                    nums[k++]= nums2[j++];
-                   
-                }
+        int[] nums = new int[m+n];
+        int i =0 , j =0 , k =0;
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j]){
+                nums[k++] =  nums1[i++];
             }
-            while(i<m){
-                nums[k++]=nums1[i++];
-                
+            else{
+                nums[k++] =  nums2[j++];
             }
-            while(j<n){
-                nums[k++] = nums2[j++];
-               
-            }
-            for(int l=0;l<nums.length;l++){
-                nums1[l] =nums[l];
-            }
-        
+        }
+        while(i < m){
+            nums[k++] =  nums1[i++];
+        }
+        while(j < n){
+            nums[k++] =  nums2[j++];
+        }
+        int index = 0;
+        for(int num:nums){
+            nums1[index++] =  num;
+        }
+
         
     }
 }
