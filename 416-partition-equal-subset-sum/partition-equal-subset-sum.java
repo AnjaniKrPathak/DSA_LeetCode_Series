a987cdf6ca53190a5e54c83b735a434d70710f7f
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2025-08-10 12:21:40 +0530

    Time: 52 ms (44.00%) | Memory: 47 MB (52.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,22 +1,37 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-      int totalSum  =0;
-      for(int num:nums){
-        totalSum += num;
-      }
-      int target = totalSum /2;
-      if(totalSum %2 !=0){
+
+        int n =  nums.length;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum +=  nums[i];
+        }
+        if(sum % 2 != 0){
             return false;
         }
-        
-        boolean[] dp = new boolean[target+1];
-        dp[0] = true;
-        for(int num:nums){
-            for(int i = target ;i>=num;i--) {
-                dp[i] = dp[i] || dp[i-num];
+        int target = sum /2 ;
+        boolean[][] dp  =  new boolean[n+1][target+1];
+        for(int i=0;i<n+1;i++){
+            for(int j= 0;j< target +1 ; j++){
+                
+                if(j == 0){
+                    dp[i][0] = true;
+                }
             }
         }
-        return dp[target];
+
+        for(int i = 1;i < n+1;i++){
+            for(int j =1 ;j<  target+1;j++){
+                if(nums[i-1] <= j){
+                    dp[i][j]  = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                }
+                else{
+                    dp[i][j] =  dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][target];
+        
     }
-  
 }
