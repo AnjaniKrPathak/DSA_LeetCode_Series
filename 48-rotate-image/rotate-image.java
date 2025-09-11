cef8d9404322229fcc5e057128f0a369731e60c5
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2025-09-11 22:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (59.96%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n =matrix.length;
+        int m=matrix[0].length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int temp =matrix[i][j];
+                matrix[i][j] =  matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int low =0; 
+            int heigh = n-1;
+            while(low <heigh){
+                int temp = matrix[i][low];
+                matrix[i][low]=matrix[i][heigh];
+                matrix[i][heigh] = temp;
+                low++;
+                heigh--;
+
+            }
+        }
+        
+       
+        
+    }
+}
