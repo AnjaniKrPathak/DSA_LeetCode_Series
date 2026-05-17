d4dd29697097e06e0d68c277346ab2e9b3db974e
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2026-05-17 11:49:19 +0530

    Time: 6 ms (13.15%) | Memory: 49 MB (5.74%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int low =0;
-        int heigh = numbers.length-1;
-        int[] res =new int[2];
-        int sum = 0;
-        while(low < heigh){
-            sum = numbers[low] +  numbers[heigh];
-            if(sum == target){
-                res[0] = low+1;
-                res[1] =  heigh +1;
-                break;
-            }
-            if(sum > target){
-                heigh--;
-            }
-            if(sum < target){
-                low++;
-            }
-            
-
+       Map<Integer,Integer> map =new HashMap<>();
+      int[] res = new int[2];
+      int n = numbers.length;
+      for(int i =0;i<n;i++){
+         int complement = target - numbers[i];
+          if (map.containsKey(complement)) {
+            return new int[]{map.get(complement)+1, i+1};
         }
-        return res;
+       
+            map.put(numbers[i],i);
+        }
+
         
+      
+      return new int[]{-1, -1};
+    
+    
+
+
     }
 }
