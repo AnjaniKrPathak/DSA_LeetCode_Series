54e558bd476a891432a49d55ac10334e15873082
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2026-05-28 21:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (74.66%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -18,16 +18,15 @@ class Solution {
         }
         if(fast == null){
             return head.next;
-
         }
-
         while(fast.next != null){
             slow = slow.next;
             fast = fast.next;
         }
-        slow.next = slow.next.next;
+        slow.next =slow.next.next;
 
         return head;
+
         
     }
 }
