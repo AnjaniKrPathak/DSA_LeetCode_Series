733947727076caebc8e7256b9ff536332c0ce711
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2026-05-29 09:09:50 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (90.48%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,19 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low =0 , heigh = nums.length-1;
-        
+
+        int low =0;
+        int heigh = nums.length-1;
         while(low <= heigh){
-            int mid = (low + heigh) / 2;
-            if(nums[mid] ==  target){
+            int mid = (low +heigh )/2;
+            if(nums[mid] == target){
                 return mid;
             }
-            if(nums[mid] < target){
-                low = mid+1;
+            else if(nums[mid] < target){
+                low = mid +1;
             }
             else{
                 heigh = mid-1;
             }
         }
+
 
         return -1;
         
