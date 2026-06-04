82d06e6e4d31c494c69a789112839a9119516838
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2026-06-04 23:55:22 +0530

    Time: 14 ms (27.79%) | Memory: 46.3 MB (44.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,28 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+        if(s.length() !=  t.length()){
             return false;
         }
-        char[] schar= s.toCharArray();
-        char[] tchar = t.toCharArray();
         Map<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<schar.length;i++){
-            map.put(schar[i],map.getOrDefault(schar[i],0)+1);
-        }
-        for(int i=0;i<tchar.length;i++){
-            char ch =tchar[i];
-              if (!map.containsKey(ch)) {
-                return false;
-            }
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)-1);
-            }
-             if (map.get(ch) == 0){
-                map.remove(ch);
-            }
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
 
-        return map.isEmpty();
+        for(char ch:t.toCharArray()){
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch) == 0){
+                    map.remove(ch);
+                }
+            }
+            
+        }
+
+        return map.size() ==0;
         
     }
 }
