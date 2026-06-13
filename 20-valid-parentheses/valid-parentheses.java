ba4d229236a2882d08f4dcf03d1209441a9bc8eb
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2026-06-13 07:46:11 +0530

    Time: 4 ms (38.89%) | Memory: 43.6 MB (9.92%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,32 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack =  new Stack<>();
+        Stack<Character> stack = new Stack<>();
         for(int i=0;i<s.length();i++){
-           
-             if(s.charAt(i) =='(' || s.charAt(i) == '{' || s.charAt(i) == '[' ){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
                 stack.push(s.charAt(i));
             }
             else{
                 if(stack.isEmpty()){
                     return false;
                 }
-                 if((s.charAt(i) == ')' && stack.peek() == '(') || (s.charAt(i) == '}' && stack.peek() == '{') 
-                || (s.charAt(i) == ']' && stack.peek() == '[')  && !stack.isEmpty()){
-                stack.pop();
+                else if((s.charAt(i) ==')' && stack.peek() == '(') ||
+                 (s.charAt(i) == '}' && stack.peek() == '{') ||
+                  (s.charAt(i) == ']' && stack.peek() == '[') && !stack.isEmpty()){
+                    stack.pop();
+                } else{
+                    return false;
+                }
             }
-            else{
-                return false;
-            }
-            }
-             
-            
-           
-            
-
         }
 
         return stack.isEmpty();
-        
         
     }
 }
