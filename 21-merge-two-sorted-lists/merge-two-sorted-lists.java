bf6082dc770de39dcf18ddaff24ea08ade4b9496
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2026-06-21 16:50:36 +0530

    Time: 1 ms (3.68%) | Memory: 44.3 MB (38.02%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,26 +10,44 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        PriorityQueue<Integer> minHeap   = new PriorityQueue<>();
-        while(list1 != null){
-            minHeap.offer(list1.val);
-            list1 = list1.next;
-        }
-        while(list2 !=  null) {
-            minHeap.offer(list2.val);
-            list2 = list2.next;
-        }
-
         ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
 
-        while(!minHeap.isEmpty()){
-            current.next = new ListNode(minHeap.poll());
-            current = current.next;
-        }
+        //  edge case 1
+         if(list1 ==  null){
+            return list2;
+         }
+         // edge case 2
+         if(list2  ==  null){
+            return list1;
+         }
 
-        return dummy.next;
-        
+         while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                current.next = new ListNode(list1.val);
+                list1 = list1.next;
+            }else{
+                current.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            current = current.next;
+         }
+         // still list1 have value
+         while(list1 != null){
+            current.next = new ListNode(list1.val);
+            current = current.next;
+            list1 = list1.next;
+         }
+         // still list2 has value
+         while(list2 != null){
+            current.next = new ListNode(list2.val);
+            current = current.next;
+            list2 = list2.next;
+
+         }
+
+         return dummy.next;
+
+          
     }
 }
