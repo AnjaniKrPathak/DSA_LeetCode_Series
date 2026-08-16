8fd4d72a6df831be03a3b4ef95eaaafa618459d8
Anjani Kumar Pathak <eranjanipathak.it@gmail.com>
2026-08-16 17:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (53.02%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -19,10 +19,10 @@ class Solution {
         if(p == null && q == null){
             return true;
         }
-        if(p== null || q == null){
+        if(p== null || q == null || p.val != q.val){
             return false;
         }
-         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right) && p.val == q.val;
+         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right) ;
 
         
     }
